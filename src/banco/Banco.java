be9cd3a421645fbfa4a415052ff18c811e3c9f23
Luzenildo Junior <luzenildojunior@lavid.ufpb.br>
2017-04-16 17:10:16 -0300

    Class banco done

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,5 +1,54 @@
 package banco;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class Banco {
 
+	Map<Conta, Cliente> cadastros = new HashMap<Conta, Cliente>();
+	
+	public void inserir(Conta conta, Cliente cliente){
+		cadastros.put(conta, cliente);
+	}
+	
+	public Conta buscaConta(String numero){
+		Set<Entry<Conta, Cliente>> set = cadastros.entrySet();
+		Iterator i = set.iterator();
+		while(i.hasNext()){
+			Entry<Conta, Cliente> entry = (Entry)i.next();
+			if(entry.getKey().getNumero() == numero){
+				return entry.getKey();
+			}
+		}
+		return null;
+	}
+	
+	public Cliente buscaCliente(String cpf){
+		Set<Entry<Conta, Cliente>> set = cadastros.entrySet();
+		Iterator i = set.iterator();
+		while(i.hasNext()){
+			Entry<Conta, Cliente> entry = (Entry)i.next();
+			if(entry.getValue().getCpf() == cpf){
+				return entry.getValue();
+			}
+		}
+		return null;
+	}
+	
+	public ArrayList buscaContasdeumCliente(String cpf){
+		ArrayList<Conta> contasDoCliente = new ArrayList<Conta>();
+		Set<Entry<Conta, Cliente>> set = cadastros.entrySet();
+		Iterator i = set.iterator();
+		while(i.hasNext()){
+			Entry<Conta, Cliente> entry = (Entry)i.next();
+			if(entry.getValue().getCpf() == cpf){
+				contasDoCliente.add(entry.getKey());
+			}
+		}
+		return contasDoCliente;
+	}
 }
